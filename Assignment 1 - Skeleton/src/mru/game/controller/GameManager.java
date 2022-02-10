06e1db34200dad81f6c4f8b2c40ef2b047e0ba1d
DBARKU <dbark913@mtroyal.ca>
2022-02-09 18:27:21 -0700

    hi

--- Assignment 1 - Skeleton/src/mru/game/controller/GameManager.java
+++ Assignment 1 - Skeleton/src/mru/game/controller/GameManager.java
@@ -91,7 +91,7 @@ public class GameManager {
 				topPlayer = p;
 			} else if(topPlayer.getNumberOfWins()<p.getNumberOfWins()) {
 				topPlayer = p;
-				System.out.print("t");
+				
 			}
 		}
 		return topPlayer;
